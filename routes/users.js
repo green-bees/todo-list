3a426a295082d2597449804bd7134dd7ef31d48d
Brian West <brianwilliamwest@gmail.com>
2016-06-27 10:03:01 -0400

    Set up new user-support branch

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+// Initial commit for user-support branch
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
