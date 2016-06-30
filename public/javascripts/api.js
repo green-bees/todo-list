7a9873b7a9aa0dcf4d45bcc44b1863013ce52684
Brian West <brianwilliamwest@gmail.com>
2016-06-30 11:42:35 -0400

    Add ability to get and display tasks

--- public/javascripts/api.js
+++ public/javascripts/api.js
@@ -1,38 +1,60 @@
-var api = {
-  getTask: function() { //??Passing IDs
-    $.getJSON('api/:id', function(data) {
-      console.log(data);
-    }
-  },
-  newTask: function() {
-    $.ajax({
-      url: 'api',
-      type: 'POST',
-      data: JSON.stringify(task), //??Add task string somehow
-      contentType: 'application/json',
-      success: function(result) {
-          console.log(result);
-      }
-    });
-  },
-  updateTask: function() {
-    $.ajax({
-      url: 'api/:id',
-      type: 'PUT',
-      data: JSON.stringify(task), //??Add task string somehow
-      contentType: 'application/json',
-      success: function(result) {
-          console.log(result);
-      }
-    });
-  },
-  deleteTask: function() { //Not sure if this is right
-    $.ajax({
-      url: 'api/:id',
-      type: 'DELETE',
-      success: function(result) {
-        console.log(result);
-      }
-    })
-  }
+// var api = {
+//   getTask: function() { //??Passing IDs
+//     $.getJSON('api/:id', function(data) {
+//       console.log(data);
+//     }
+//   },
+//   newTask: function() {
+//     $.ajax({
+//       url: 'api',
+//       type: 'POST',
+//       data: JSON.stringify(task), //??Add task string somehow
+//       contentType: 'application/json',
+//       success: function(result) {
+//           console.log(result);
+//       }
+//     });
+//   },
+//   updateTask: function() {
+//     $.ajax({
+//       url: 'api/:id',
+//       type: 'PUT',
+//       data: JSON.stringify(task), //??Add task string somehow
+//       contentType: 'application/json',
+//       success: function(result) {
+//           console.log(result);
+//       }
+//     });
+//   },
+//   deleteTask: function() { //Not sure if this is right
+//     $.ajax({
+//       url: 'api/:id',
+//       type: 'DELETE',
+//       success: function(result) {
+//         console.log(result);
+//       }
+//     })
+//   }
+// };
+
+'use strict';
+
+var api = {};
+
+api.url = '/api/';
+
+// Get all tasks
+api.getTasks = function(callback) {
+  var config = {
+    method: 'GET',
+    url: api.url
+  };
+
+  $.ajax(config).done(function(tasks) {
+    callback(tasks);
+  });
 };
+
+// Testing purposes
+(function() {
+})();
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,40 +1,63 @@
-Vue.component('tasks', {
-  template: '#tasks-template',
+// Vue.component('tasks', {
+//   template: '#tasks-template',
+//
+//   data: function() {
+//     return {
+//       list: []
+//     }
+//   },
+//
+//   created: function() {
+//     $.getJSON('api', function(tasks) {    //get request for full tasks list
+//       this.list = tasks;
+//     }.bind(this))
+//   },
+//
+//   methods: {
+//     deleteTask: function(task) {    //delete method. no request yet
+//       this.list.$remove(task);
+//     }
+//   }
+// })
+//
+// Vue.component('task', {
+//   template: '#task-template'
+// })
+//
+// Vue.component('newTask', {
+//   template: '#newTask-template'
+// })
+//
+// new Vue({
+//   el: '#app',
+//
+//   data: {           //default values for new task form use
+//     task: '',
+//     importance: 1,
+//     completed: false
+//   }
+//
+// })
 
-  data: function() {
-    return {
-      list: []
-    }
-  },
+var taskList = new Vue({
+  el: '#taskList',
 
-  created: function() {
-    $.getJSON('api', function(tasks) {    //get request for full tasks list
-      this.list = tasks;
-    }.bind(this))
+  data: {
+    tasks: []
   },
 
   methods: {
-    deleteTask: function(task) {    //delete method. no request yet
-      this.list.$remove(task);
+    getTasks: function() {
+      var self = this;
+
+      api.getTasks(function(tasks) {
+        self.tasks = [];
+        tasks.forEach(function(task) {
+          self.tasks.push(task);
+        });
+      });
     }
   }
-})
+});
 
-Vue.component('task', {
-  template: '#task-template'
-})
-
-Vue.component('newTask', {
-  template: '#newTask-template'
-})
-
-new Vue({
-  el: '#app',
-
-  data: {           //default values for new task form use
-    task: '',
-    importance: 1,
-    completed: false
-  }
-
-})
+taskList.getTasks();
