1cd14cdf23cbef00f54700a8f358f28a53ee0682
Jon Heller <jon.heller02@gmail.com>
2016-07-02 21:56:04 -0400

    remove old Task Submission markup code

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,30 +1,3 @@
-var taskSubmit = new Vue({
-  el: '#taskSubmit',
-
-  data: {
-    taskDesc: ''
-  },
-
-  methods: {
-    addTask: function() {
-      var self = this;
-      var task = this.taskDesc;
-      var importance = 1; // Default value 1 (TODO: allow for input)
-
-      var task = {
-        task: task,
-        importance: importance,
-        completed: false
-      };
-
-      api.addTask(task, function(status) { // The status variable is an http status
-        refreshTasks();
-        self.taskDesc = '';
-      });
-    }
-  }
-});
-
 var taskList = new Vue({
   el: '#taskList',
 
